8eaf3c7577109757d93051ecd5ab0dac06c5fc55
Antonio Ortells <antonio.ortells@okta.com>
2024-06-25 12:12:13 +0200

    First Baseline

--- index.js
+++ index.js
@@ -5,33 +5,14 @@ var request = require('request');
 var app = express();
 const port = 3000;
 
-const BASEURL='http://dev-antonio-ortells.eu.auth0.com';
-
 app.get('/', function (req, res) {
-  console.log('Requested root')
-  res.status(200).send(
-    {
-      applinks: {
-        apps: [],
-        details: [
-          {
-            appID: "9JA89QQLNQ.com.my.auth.bundle",
-            paths: ["/ios/com.my.auth.bundle/*"]
-          }
-        ]
-      }
-    }
-  );
-});
-
-app.get('/*', function (req, res) {
-  console.log(`Requested ${req.url}`)
-  var newurl = `${BASEURL}${req.url}`;
-  res.redirect(newurl);
+  console.log('Requested Callback - Angular (4200)')
+  let url = `http://localhost:4200/${req.originalUrl}`
+  res.redirect(url);
 });
 
 app.listen(port, () => {
-  console.log(`Apple App Site Association listening on port ${port}`)
+  console.log(`Localhost Redir listening on port ${port}`)
 })
 
 module.exports = Webtask.fromExpress(app);
